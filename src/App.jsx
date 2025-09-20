43c581adb4babda40e678d59b9bcf0b3e1c845f4
1m4nim <fancustard9@gmail.com>
2025-09-20 11:09:03 +0900

    JavaScriptの予約語を避けてJSX内で記述する

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// // function App() {
+// //   return <h1>JSXの基本</h1>;
+// // }
+// // export default App;
+
+// // const App = () => {
+// //   return <h1>JSXの基本</h1>;
+// // };
+// // export default App;
+
+// function App() {
+//   return (
+//     <>
+//       <h1>JSXの基本</h1>
+//       <p>JSXのルート要素</p>
+//     </>
+//   );
+// }
+// export default App;
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>JSXの基本</h1>
+//       <p>JSXのルート要素</p>
+//     </>
+//   );
+// };
+// export default App;
+
+// import { Fragment } from "react";
+// const App = () => {
+//   return (
+//     <Fragment>
+//       <h1>JSXの基本</h1>
+//       <p>JSXのルート要素</p>
+//     </Fragment>
+//   );
+// };
+// export default App;
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1 className="font-normal">JSXの基本</h1>
+//     </>
+//   );
+// };
+// export default App;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="font-normal">JSXの基本２</h1>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
