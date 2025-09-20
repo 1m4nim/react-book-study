655b1ef74cacaf2d582bf19e973ea5b0abc2f4d5
1m4nim <fancustard9@gmail.com>
2025-09-20 16:41:11 +0900

    三項演算子・map()・条件式をやった

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// // function App() {
-// //   return <h1>JSXの基本</h1>;
-// // }
-// // export default App;
+//アロー関数
+// function App() {
+//   return <h1>JSXの基本</h1>;
+// }
+// export default App;
 
-// // const App = () => {
-// //   return <h1>JSXの基本</h1>;
-// // };
-// // export default App;
+// const App = () => {
+//   return <h1>JSXの基本</h1>;
+// };
+// export default App;
 
 // function App() {
 //   return (
@@ -28,6 +29,7 @@
 // };
 // export default App;
 
+//Reactフラグメント
 // import { Fragment } from "react";
 // const App = () => {
 //   return (
@@ -39,6 +41,7 @@
 // };
 // export default App;
 
+//className属性
 // const App = () => {
 //   return (
 //     <>
@@ -48,11 +51,103 @@
 // };
 // export default App;
 
+// function App() {
+//   return (
+//     <>
+//       <h1 className="font-normal">JSXの基本２</h1>
+//     </>
+//   );
+// }
+// export default App;
+
+//JSX内ではJSが記述可能
+// function App() {
+//   const text = "aiueo";
+//   return (
+//     <>
+//       <h1>{text}</h1>
+//     </>
+//   );
+// }
+// export default App;
+
+// function App() {
+//   const h1className = "font-normal";
+//   return (
+//     <>
+//       <h1 className={h1className}>aiueo</h1>
+//     </>
+//   );
+// }
+// export default App;
+
+//スプレッド構文を利用した属性値の指定
+// function App() {
+//   const attributes = {
+//     id: "hoge",
+//     className: "piyo",
+//   };
+//   return (
+//     <>
+//       <h1 {...attributes}>poyopoyo</h1>
+//     </>
+//   );
+// }
+// export default App;
+
+//&&演算子を利用した条件分岐
+// function App() {
+//   const hasLead = true;
+//   return (
+//     <>
+//       <h1>piyo</h1>
+//       {hasLead && <p>JavaScript</p>}
+//     </>
+//   );
+// }
+// export default App;
+
+//三項演算子を利用した条件分岐
+//条件式　?　正しいときの処理　：　正しくないときの処理
+//isDeletdがtrueのときが<del>
+//falseだと<p>
+
+// function App() {
+//   const isDsleted = true;
+//   return (
+//     <>
+//       <h1>poyopoyo</h1>
+//       {isDsleted ? <del>JavaScript</del> : <p>JavaScript</p>}
+//     </>
+//   );
+// }
+// export default App;
+
+//map()を利用した繰り返し処理
+// function App() {
+//   const fruits = ["apple", "banana", "peach", "orange"];
+//   return (
+//     <ul>
+//       {fruits.map((fruits) => {
+//         return <li>{fruits}</li>;
+//       })}
+//     </ul>
+//   );
+// }
+// export default App;
+
+//条件文と繰り返し
 function App() {
+  const myfavoriteFruits = "orange";
+  const fruits = ["apple", "banana", "orange", "peach"];
   return (
-    <>
-      <h1 className="font-normal">JSXの基本２</h1>
-    </>
+    <ul>
+      {fruits.map((fruit, index) => (
+        <li key={index}>
+          {fruit === myfavoriteFruits ? <strong>{fruit}</strong> : fruit}
+        </li>
+      ))}
+    </ul>
   );
 }
 export default App;
